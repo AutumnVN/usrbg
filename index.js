246ebefe63de3e8eaa8cc6e02b5f37eb4389b371
AutumnVN <htuan03@gmail.com>
2023-04-16 06:53:10 +0700

    json

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ import { writeFileSync } from 'fs';
 import fetch from 'node-fetch';
 
 const URL = 'https://raw.githubusercontent.com/Discord-Custom-Covers/usrbg/master/dist/usrbg.json';
+let usrbg = {};
 
 fetch(URL)
     .then(response => response.json())
     .then(data => {
         for (const i of data) {
             writeFileSync(`./dist/${i.uid}.txt`, i.img);
+            usrbg[i.uid] = i.img;
         }
+        writeFileSync('./usrbg.json', JSON.stringify(usrbg));
     });
