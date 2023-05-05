56813148a3b4ff2433231a491a481e05ddb2a033
AutumnVN <htuan03@gmail.com>
2023-05-05 07:13:22 +0700

    filter out dead banner links

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { writeFileSync } from 'fs';
 import fetch from 'node-fetch';
+import deadbanner from './deadbanner.json';
 
 const URL = 'https://raw.githubusercontent.com/Discord-Custom-Covers/usrbg/master/dist/usrbg.json';
 let usrbg = {};
@@ -8,6 +9,7 @@ fetch(URL)
     .then(response => response.json())
     .then(data => {
         for (const i of data) {
+            if (deadbanner.includes(i.img)) continue;
             usrbg[i.uid] = i.img;
         }
         writeFileSync('./usrbg.json', JSON.stringify(usrbg));
