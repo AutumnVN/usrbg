c9b7a4ed219f01d7b51a91d169f3377531f1beb6
AutumnVN <autumnvnchino@gmail.com>
2023-10-06 11:59:26 +0700

    node 18

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-import { writeFileSync, readFileSync } from 'fs';
-import fetch from 'node-fetch';
+const { writeFileSync, readFileSync } = require('fs');
 
 const URL = 'https://raw.githubusercontent.com/Discord-Custom-Covers/usrbg/master/dist/usrbg.json';
 const deadbanner = JSON.parse(readFileSync('./deadbanner.json'));
